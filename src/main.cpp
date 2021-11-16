b8c433dcc15fbd06f3709913eaf77873b32c8136
Michael Haberler <git@mah.priv.at>
2021-11-16 02:01:00 +0100

    check if the esp32_exception_decoder works - yep it does

--- src/main.cpp
+++ src/main.cpp
@@ -2,44 +2,43 @@
 // Based on code of Nick Gammon  http://www.gammon.com.au/forum/?id=10896
 // ESP32 DevKit - Arduino IDE 1.8.5
 // Device tested PCF8574 - Use pullup resistors 3K3 ohms !
-// PCF8574 Default Freq 100 KHz 
+// PCF8574 Default Freq 100 KHz
 
 #include <Arduino.h>
 #include <Wire.h>
+int x = 0;
 
-void setup()
-{
-  Serial.begin (115200);  
-  Wire.begin (21, 22);   // sda= GPIO_21 /scl= GPIO_22
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(21, 22); // sda= GPIO_21 /scl= GPIO_22
 }
 
-void Scanner ()
-{
-  Serial.println ();
-  Serial.println ("I2C scanner. Scanning ...");
+void Scanner() {
+  Serial.println();
+  Serial.println("I2C scanner. Scanning ...");
   byte count = 0;
 
   Wire.begin();
-  for (byte i = 8; i < 120; i++)
-  {
-    Wire.beginTransmission (i);          // Begin I2C transmission Address (i)
-    if (Wire.endTransmission () == 0)  // Receive 0 = success (ACK response) 
+  for (byte i = 8; i < 120; i++) {
+    Wire.beginTransmission(i);       // Begin I2C transmission Address (i)
+    if (Wire.endTransmission() == 0) // Receive 0 = success (ACK response)
     {
-      Serial.print ("Found address: ");
-      Serial.print (i, DEC);
-      Serial.print (" (0x");
-      Serial.print (i, HEX);     // PCF8574 7 bit address
-      Serial.println (")");
+      Serial.print("Found address: ");
+      Serial.print(i, DEC);
+      Serial.print(" (0x");
+      Serial.print(i, HEX); // PCF8574 7 bit address
+      Serial.println(")");
       count++;
     }
   }
-  Serial.print ("Found ");      
-  Serial.print (count, DEC);        // numbers of devices
-  Serial.println (" device(s).");
+
+  Serial.print("Found ");
+  Serial.print(count, DEC); // numbers of devices
+  Serial.println(" device(s).");
 }
 
-void loop()
-{
-  Scanner ();
-  delay (1000);
+void loop() {
+  Scanner();
+  float foo = 1 / x;
+  delay(1000);
 }
